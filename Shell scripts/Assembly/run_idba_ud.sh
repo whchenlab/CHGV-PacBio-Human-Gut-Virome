0d8567fab8f8405e59a14a0f7bd521c97801bc25
Chuqing Sun <41618697+Zoexfq@users.noreply.github.com>
2022-12-10 18:54:20 +0800

    Create run_idba_ud.sh

---
+++ Shell scripts/Assembly/run_idba_ud.sh
@@ -0,0 +1,14 @@
+R1=$1
+R2=$2
+output=$3
+threads=$4
+sample=`basename $R1|cut -d "_" -f 1|cut -d "." -f 1`
+path=<path to idba folder>
+export PATH=$PATH:$path
+if [ ! -d $output ]
+then
+    mkdir -p $output
+fi
+fq2fa --merge --filter $R1 $R2 $output/$sample.fa
+idba_ud -r $sample.fa -o $output --num_threads $threads \
+    --maxk 120 --step 10 --min_contig 1500
